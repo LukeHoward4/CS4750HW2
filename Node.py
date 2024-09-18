daf29ea15d625d123551738d28e4cebd63a241a3
Vani Seth <sethvani2002@gmail.com>
2024-09-18 13:55:04 -0500

    added path cost funtion

--- Node.py
+++ Node.py
@@ -4,4 +4,17 @@ class Node():
         self.__state = state
         self.__action = action
         self.__pathCost = pathCost
+
+    def get_parent(self):
+        return self.__parentNode
+
+    def get_path_cost(self):
+        return self.__pathCost
+
+def calculate_path_cost(node):
+    total_cost = 0
+    while node is not None:
+        total_cost += node.get_path_cost()
+        node = node.get_parent()
+    return total_cost
     
