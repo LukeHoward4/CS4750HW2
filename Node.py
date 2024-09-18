681542722c0d0b5feee911458b6a48e5b46350be
lukehoward4 <lwhby7@umsystem.edu>
2024-09-18 11:23:15 -0500

    Defined Node class in node.py and made basic initializer

--- Node.py
+++ Node.py
@@ -1,1 +1,7 @@
-
+class Node():
+    def __init__(self, parentNode, state, action, pathCost):
+        self.__parentNode = parentNode
+        self.__state = state
+        self.__action = action
+        self.__pathCost = pathCost
+    
